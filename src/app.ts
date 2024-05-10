95a3fd7d09b3633741c54d6429f15749991bc861
Manya7547 <52461524+Manya7547@users.noreply.github.com>
2024-05-10 12:41:28 -0700

    add global error handler

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,9 @@
-import express from 'express'
+import express, { NextFunction } from 'express'
+import { Request } from 'express';
+import {Response} from 'express';
+import createHttpError, {HttpError} from 'http-errors';
+import { config } from './config/config';
+import globalErrorHandler from './middlewares/globalErrorHandler';
 
 const app = express();
 
@@ -6,7 +11,15 @@ const app = express();
 //Routes or URLs
 
 app.get('/',(req, res, next) => {
+
+    const error = createHttpError(400, "something went wrong");
+    throw error;
     res.json({message: "Welcome to ELibrary"})
+
 })
+
+
+//global error handler
+app.use(globalErrorHandler);
 
 export default app
