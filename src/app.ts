8512adc1f2883f2fc726e790f6aee0d885bb5315
Manya7547 <52461524+Manya7547@users.noreply.github.com>
2024-05-10 18:22:49 -0700

    add book model

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express, { NextFunction } from 'express'
 import { Request } from 'express';
 import {Response} from 'express';
 import createHttpError, {HttpError} from 'http-errors';
+import bookRouter from './book/bookRouter';
 import { config } from './config/config';
 import globalErrorHandler from './middlewares/globalErrorHandler';
 import userRouter from './user/userRouter';
@@ -20,7 +21,9 @@ app.get('/',(req, res, next) => {
 
 })
 
+//register the router
 app.use("/api/users",userRouter);
+app.use("/api/books",bookRouter);
 
 
 //global error handler
